2a6a9c58a4df592578641b26f48d280f6d468256
tci <cichymail@gmail.com>
2018-02-24 16:54:25 +0100

    Consistent naming

--- stepper/stepper2.fs
+++ stepper/stepper2.fs
@@ -3,15 +3,15 @@ PB8 constant M2A
 PB7 constant M3A
 PB6 constant M4A
 
-%11 constant motor-fullsteps-mask
-create motor-fullsteps \ full steps
+%11 constant motor.fullsteps-mask
+create motor.fullsteps \ full steps
 %1010 c,
 %0110 c,
 %0101 c,
 %1001 c,
 
-%111 constant motor-halfsteps-mask
-create motor-halfsteps \ half steps
+%111 constant motor.halfsteps-mask
+create motor.halfsteps \ half steps
 %1010 c,
 %0010 c,
 %0110 c,
@@ -22,17 +22,17 @@ create motor-halfsteps \ half steps
 %1000 c,
 
 1 variable motor.mode \ 0 - full steps | 1 - half steps
-: motor-mask ( -- mask )
+: motor:mask ( -- mask )
   motor.mode @
-  if   motor-halfsteps-mask
-  else motor-fullsteps-mask
+  if   motor.halfsteps-mask
+  else motor.fullsteps-mask
   then
 ;
 
-: motor-step-bits ( n -- bits )
+: motor:step-bits ( n -- bits )
   motor.mode @
-  if   motor-halfsteps
-  else motor-fullsteps
+  if   motor.halfsteps
+  else motor.fullsteps
   then + c@
 ;
 
@@ -40,7 +40,7 @@ create motor-halfsteps \ half steps
 0 variable motor.phase
 
 \ change working mode and correct phase number
-: motor-set-mode ( mode -- )
+: motor:set-mode ( mode -- )
   dup ( mode mode )
   2* motor.mode @ or ( mode new_mode|old_mode-as-bits )
   dup ( mode new_mode|old_mode-as-bits x2 )
@@ -60,8 +60,8 @@ create motor-halfsteps \ half steps
   motor.mode !
 ;
 
-: motor-on ( -- )
-  motor.phase @ motor-step-bits \ read current step bits
+: motor:on ( -- )
+  motor.phase @ motor:step-bits \ read current step bits
   dup 1 and if M1A ios! else M1A ioc! then
   2/
   dup 1 and if M2A ios! else M2A ioc! then
@@ -71,28 +71,28 @@ create motor-halfsteps \ half steps
       1 and if M4A ios! else M4A ioc! then
 ;
 
-: motor-off ( -- )
+: motor:off ( -- )
   M1A ioc!
   M2A ioc!
   M3A ioc!
   M4A ioc!
 ;
 
-: motor.init ( -- )
+: motor:init ( -- )
   omode-pp M1A io-mode!
   omode-pp M2A io-mode!
   omode-pp M3A io-mode!
   omode-pp M4A io-mode!
-  motor-off
+  motor:off
 ;
 
-: motor-step ( direction -- )
-  motor.phase @ + motor-mask and \ change phase`
+: motor:step ( direction -- )
+  motor.phase @ + motor:mask and \ change phase`
   motor.phase ! \ update
-  motor-on \ set output accordingly
+  motor:on \ set output accordingly
 ;
 
-: motor-print ( -- )
+: motor:print ( -- )
   motor.phase @ .
   M1A io@ .
   M2A io@ .
@@ -101,16 +101,16 @@ create motor-halfsteps \ half steps
   cr
 ;
 
-: motor-move ( n -- )
+: motor:move ( n -- )
   dup 0< if -1 else 1 then ( n sign[n] )
   swap abs ( sign[n] |n| )
   0 do
-    dup motor-step
-    \ motor-print
+    dup motor:step
+    \ motor:print
     motor.delay @ ms
   loop ( sign[n] )
   drop
-  motor-off
+  motor:off
 ;
 
-motor.init
+motor:init
